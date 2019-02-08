1b2d5e9dfe5bac255ba1d99dbc0f40cfffcfc642
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2019-02-08 13:24:13 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿/* Ethan Shipston
+ * 2/6/2019
+ * Hello world my first and fav  program
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184988HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            message.Content = "Hello, " + name;
+        }
+    }
+}
